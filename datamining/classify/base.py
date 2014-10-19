709e3c12a9e83aaddcc0608ad91f89d519ede48f
RobinTang <sintrb@gmail.com>
2014-10-20 01:19:37 +0800

    添加分类器测试函数

--- datamining/classify/base.py
+++ datamining/classify/base.py
@@ -19,3 +19,24 @@ class BaseClassifier(object):
 		"""对数据进行分类，返回该数据的分类"""
 		clss  = self.classify(sample)
 		return max(clss.items(), key=lambda x:x[1])[0]
+
+def test_classifer(clf):
+	'''
+	测试分类器
+	'''
+	# 分类训练
+	clf.training('abcdefghijklmnopqrst', 0)
+	clf.training('aab', 0)
+	clf.training('a', 1)
+	clf.training('d', 1)
+	clf.training('e', 2)
+	
+# 	db.show_info()
+
+	# 分类测试
+	print clf.whichclass('a')
+	print clf.whichclass('c')
+	print clf.whichclass('d')
+	print clf.whichclass('t')
+	print clf.whichclass('e')
+	print clf.whichclass('opqrst')
