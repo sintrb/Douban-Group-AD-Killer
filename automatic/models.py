68414dd11c8c32272e8d10b1c299d1183f70efe1
RobinTang <sintrb@gmail.com>
2014-10-28 22:57:52 +0800

    帖子模型

--- automatic/models.py
+++ automatic/models.py
@@ -1,3 +1,16 @@
+# -*- coding: UTF-8 -*
+'''
+Created on 2014年10月28日
+
+@author: RobinTang
+'''
+
 from django.db import models
 
-# Create your models here.
+class GroupTopic(models.Model):
+    tid = models.CharField(max_length=16)
+    url = models.CharField(max_length=16)
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    type = models.CharField(max_length=16)
+    other = models.CharField(max_length=255)
