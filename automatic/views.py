4c2ba4a12420c19b9af0673be0ea00d2e4ef886a
RobinTang <sintrb@gmail.com>
2014-11-03 19:10:49 +0800

    自动举报

--- automatic/views.py
+++ automatic/views.py
@@ -10,7 +10,7 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 
-from doubapi import get_topic_list, get_topic
+from doubapi import get_topic_list, get_topic, comment_topic, report_topic
 from models import GroupTopic
 
 from datamining.funs import cut
@@ -32,7 +32,7 @@ def path(patrn, *args):
 
 
 def get_topicatbs(topic):
-    return [t for t in cut(topic.title+' '+topic.text) if not t in ignor]
+    return [t for t in cut(topic.title + ' ' + topic.text) if not t in ignor]
 
 
 @path('^newtopic/')
@@ -45,7 +45,7 @@ def new_topic(request):
     count = 5
     for tid in ids:
         jt = get_topic(tid)
-        t = GroupTopic(tid=tid, 
+        t = GroupTopic(tid=tid,
                        url=jt['alt'],
                        uid=jt['author']['id'],
                        gid=jt['group']['id'],
@@ -55,6 +55,9 @@ def new_topic(request):
                        type='',
                        other='',
                        )
+        if len(t.text) < 10 or len(t.text) > 1000:
+            # 忽略过长过过短的
+            t.type = 'i'
         t.save()
         ts.append(t)
         count = count - 1
@@ -79,7 +82,7 @@ def training_ignor_class(request, tid):
     sample = get_topicatbs(t)
     nbc = NaiveBayesClassifier(Drive())
     r = nbc.classify(sample)
-    res = '%s %s'%(r, time.time()-st)
+    res = '%s %s' % (r, time.time() - st)
     return HttpResponse(res)
 
 @path('^training/([0-9]+)/([a-z]+)')
@@ -92,27 +95,71 @@ def training_to_class(request, tid, cls):
     t.other = 'train'
     t.type = cls
     t.save()
-    res = '%s %s'%('/'.join(sample), time.time()-st)
+    res = '%s %s' % ('/'.join(sample), time.time() - st)
     return HttpResponse(res)
 
 @path('^training/([a-z]*)')
 def training(request, act=None):
     ts = GroupTopic.objects.filter()
-    if act!='list':
+    if act != 'list':
         ts = ts.filter(type='')
     ts = ts[0:50]
     cxt = RequestContext(request)
     cxt['topics'] = ts
     cxt['types'] = [
-                    ('ok','正常'),
-                    ('ad','广告'),
-                    ('po','色情'),
-                    ('co','求安慰'),
-                    ('ml','约贴'),
-                    ('q','测试'),
-                    ('i','忽略'),
+                    ('ok', '正常'),
+                    ('ad', '广告'),
+                    ('po', '色情'),
+                    ('co', '求安慰'),
+                    ('ml', '约贴'),
+                    ('q', '测试'),
+                    ('i', '忽略'),
                     ]
     return render_to_response('training.html', cxt)
+
+@path('^findad/([0-9]*)')
+def findad(request, tid=''):
+    t = None
+    if tid:
+        t = GroupTopic.objects.filter(tid=tid, other='').first()
+    else:
+        t = GroupTopic.objects.filter(type='', other='').first()
+    
+    
+    if not t:
+        return new_topic(request)
+    
+    st = time.time()
+    sample = get_topicatbs(t)
+    nbc = NaiveBayesClassifier(Drive())
+    r = nbc.classify(sample)
+    ct = time.time() - st
+    
+    t.type = max(r.items(), key=lambda x:x[1])[0]
+    p = r['ad'] / (r['ad'] + r['ok'])
+    t.other = 'auto'
+    st = time.time()
+    if p > 0.60:
+        # 广告
+        t.other = t.other + " ad" 
+        try:
+            comment_topic(t.tid, '神似广告 %s' % p)
+            report_topic(t.tid)
+        except:
+            t.other = 'failed'
+    t.save()
+    rt = time.time() - st
+    res = {
+           'title':t.title,
+           'url':t.url,
+           'tid':t.tid,
+           'p':p,
+           'type':t.type,
+           'other':t.other,
+           'class_time':ct,
+           'report_time':rt
+           }
+    return HttpResponse(json.dumps(res))
 
 # @path('.*')
 # def i(request):
