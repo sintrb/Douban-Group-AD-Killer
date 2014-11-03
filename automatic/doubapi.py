0fda5e5c6ca865713c71ee4e2f17dea96edd5d70
RobinTang <sintrb@gmail.com>
2014-11-03 18:26:23 +0800

    添加评论和举报接口

--- automatic/doubapi.py
+++ automatic/doubapi.py
@@ -12,7 +12,7 @@ token = '???'
 headers = {
         # Chrome User-Agent
         'User-Agent':'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36',
-        'Authorization': 'Bearer 7bb945a63752b9be732bd0f2a5f82d3a',
+        'Authorization': 'Bearer %s' % token,
         }
 http = HttpHolder(headers=headers, timeout=30)
 def get_topic_list():
@@ -20,11 +20,20 @@ def get_topic_list():
     return json.loads(http.open_html(url))[u'topics']
 
 def get_topic(tid):
-    url = 'https://api.douban.com/v2/group/topic/%s/'%tid
+    url = 'https://api.douban.com/v2/group/topic/%s/' % tid
     return json.loads(http.open_html(url))
 
+def report_topic(tid, reason="0"):
+    url = 'https://api.douban.com/v2/group/topic/%s/report' % tid
+    return json.loads(http.open_html(url, data={"reason":reason}))
+
+def comment_topic(tid, txt, pid=''):
+    url = 'https://api.douban.com/v2/group/topic/%s/add_comment' % tid
+    return json.loads(http.open_html(url, data={"content":txt, "comment_id":pid}))
+
 if __name__ == '__main__':
-    print get_topic_list()
+    print comment_topic('63160261', '哈哈哈')
+#     print report_topic('65201342')
 
 
 
