82e5e610a6fd3d588e5b54457b5a334628dec991
RobinTang <sintrb@gmail.com>
2014-11-04 19:46:18 +0800

    自动测试工具

---
+++ dotask.py
@@ -0,0 +1,27 @@
+# -*- coding: UTF-8 -*
+'''
+Created on 2014年11月4日
+
+@author: RobinTang
+'''
+
+
+import urllib2
+import time
+
+if __name__ == "__main__":
+    maxtime = 30
+    mintime = 5
+    timestep = 5
+    waittime = mintime
+    while True:
+        html = urllib2.urlopen('http://127.0.0.1:8080/findad/').read()
+        print html
+        if len(html) < 5 and waittime < maxtime:
+            waittime = waittime + timestep
+        else:
+            waittime = mintime
+        print 'wait %s' % waittime
+        time.sleep(waittime)
+    
+
